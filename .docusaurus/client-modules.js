a5b3e2e8ca959078d8734e12a5512189d16fea2a
alex <info@stakecraft.com>
2023-03-25 00:59:23 +0200

    some changes

--- .docusaurus/client-modules.js
+++
@@ -1,6 +0,0 @@
-export default [
-  require('/Users/novy/novy/crypto/stakecraft/stakecraft-services-website/node_modules/infima/dist/css/default/default.css'),
-  require('/Users/novy/novy/crypto/stakecraft/stakecraft-services-website/node_modules/@docusaurus/theme-classic/lib/prism-include-languages'),
-  require('/Users/novy/novy/crypto/stakecraft/stakecraft-services-website/node_modules/@docusaurus/theme-classic/lib/nprogress'),
-  require('/Users/novy/novy/crypto/stakecraft/stakecraft-services-website/src/css/custom.css'),
-];
